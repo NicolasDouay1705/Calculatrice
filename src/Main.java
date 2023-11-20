b7896bb3873a3ed1373fad52dbdd6f8383532c46
NicolasDouay17 <146863201+NicolasDouay17@users.noreply.github.com>
2023-11-20 19:47:40 +0100

    salut

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.TilePane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.text.Font;
 
 import java.awt.*;
 import java.util.regex.Pattern;
@@ -22,52 +19,91 @@ public class Main extends Application {
         primaryStage.setTitle("Calculatrice");
 
         Label resultat = new Label("Résultat");
+        resultat.setFont(Font.font("Cambria", 32));
+        resultat.setAlignment(Pos.BASELINE_RIGHT);
+        resultat.setPrefSize(380,100);
 
         Button bouton0 = new Button("0");
-        Button bouton1 = new Button("1");
-        Button bouton2 = new Button("2");
-        Button bouton3 = new Button("3");
-        Button bouton4 = new Button("4");
-        Button bouton5 = new Button("5");
-        Button bouton6 = new Button("6");
-        Button bouton7 = new Button("7");
-        Button bouton8 = new Button("8");
-        Button bouton9 = new Button("9");
-        Button virgule = new Button(",");
-        Button plus = new Button("+");
-        Button moins = new Button("-");
-        Button fois = new Button("*");
-        Button diviser = new Button(":");
-        Button egal = new Button("=");
-        Button reset = new Button("AC");
-        Button supp = new Button("C");
-        Button memoire = new Button("M+/M-");
-
-        resultat.setPrefSize(400,100);
         bouton0.setPrefSize(100,100);
+        bouton0.setFont(Font.font("Cambria",32));
+
+        Button bouton1 = new Button("1");
         bouton1.setPrefSize(100,100);
+        bouton1.setFont(Font.font("Cambria",32));
+
+        Button bouton2 = new Button("2");
         bouton2.setPrefSize(100,100);
+        bouton2.setFont(Font.font("Cambria",32));
+
+        Button bouton3 = new Button("3");
         bouton3.setPrefSize(100,100);
+        bouton3.setFont(Font.font("Cambria",32));
+
+        Button bouton4 = new Button("4");
         bouton4.setPrefSize(100,100);
+        bouton4.setFont(Font.font("Cambria",32));
+
+        Button bouton5 = new Button("5");
         bouton5.setPrefSize(100,100);
+        bouton5.setFont(Font.font("Cambria",32));
+
+        Button bouton6 = new Button("6");
         bouton6.setPrefSize(100,100);
+        bouton6.setFont(Font.font("Cambria",32));
+
+        Button bouton7 = new Button("7");
         bouton7.setPrefSize(100,100);
+        bouton7.setFont(Font.font("Cambria",32));
+
+        Button bouton8 = new Button("8");
         bouton8.setPrefSize(100,100);
+        bouton8.setFont(Font.font("Cambria",32));
+
+        Button bouton9 = new Button("9");
         bouton9.setPrefSize(100,100);
+        bouton9.setFont(Font.font("Cambria",32));
+
+        Button virgule = new Button(",");
         virgule.setPrefSize(100,100);
+        virgule.setFont(Font.font("Cambria",32));
+
+        Button plus = new Button("+");
         plus.setPrefSize(100,100);
+        plus.setFont(Font.font("Cambria",32));
+
+        Button moins = new Button("-");
         moins.setPrefSize(100,100);
+        moins.setFont(Font.font("Cambria",32));
+
+        Button fois = new Button("*");
         fois.setPrefSize(100,100);
+        fois.setFont(Font.font("Cambria",32));
+
+        Button diviser = new Button(":");
         diviser.setPrefSize(100,100);
+        diviser.setFont(Font.font("Cambria",32));
+
+        Button egal = new Button("=");
         egal.setPrefSize(200,100);
+        egal.setFont(Font.font("Cambria",32));
+
+        Button reset = new Button("AC");
         reset.setPrefSize(100,100);
+        reset.setFont(Font.font("Cambria",32));
+
+        Button supp = new Button("C");
         supp.setPrefSize(100,100);
+        supp.setFont(Font.font("Cambria",32));
+
+        Button memoire = new Button("M+/M-");
         memoire.setPrefSize(100,100);
+        memoire.setFont(Font.font("Cambria",20));
+
 
         FlowPane affichage = new FlowPane();
 
 
-        affichage.getChildren().add(resultat); // 1ere ligne
+        affichage.getChildren().add(resultat);  // 1ere ligne
         affichage.getChildren().addAll(reset,supp,memoire,diviser); // 2e ligne
         affichage.getChildren().addAll(bouton7,bouton8,bouton9,fois); // 3e ligne
         affichage.getChildren().addAll(bouton4,bouton5,bouton6,plus); // 4e ligne
